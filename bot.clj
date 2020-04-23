9ae0fb84991bfa0726a391a5dded12d16190f755
John Maruska <john.maruska@rate.com>
2020-04-23 17:11:33 -0500

    poking at clj version

---
+++ bot.clj
@@ -0,0 +1,22 @@
+(ns bot.core
+  (:require [discordian.deck :as deck
+             discord.bot :as bot]))
+
+(def playing-cards (deck/shuffle :with-jokers true))
+
+(defn parse [content]
+  (let [[opts comment] (clojure.string/split content " ! ")]
+    {:comment comment
+     :options (clojure.string/split opts " ")}))
+
+(bot/defcommand peek
+  [client message])
+
+(bot/defcommand draw
+  [client message]
+  (let [{:keys [comment options]} (parse (:content message))
+        n (int (first (options)))]
+    (deck/draw! playing-cards n)))
+
+(bot/defcommand shuffle
+  [client message])
---
+++ deck.clj
@@ -0,0 +1,35 @@
+(ns bot.deck)
+
+(def red-suits ["Hearts" "Diamonds"])
+(def black-suits ["Clubs" "Spades"])
+(def suits (concat red-suits black-suits))
+(def ranks ["Ace" "Two" "Three" "Four" "Five" "Six" "Seven" "Eight" "Nine" "Ten" "Jack" "Queen" "King"])
+
+(defn- standard-deck []
+  (for [suit suits
+        rank ranks]
+    {:suit suit :rank rank :color (if (contains? red-suits suit)
+                                    "Red" "Black")}))
+(defn- add-jokers [deck]
+  (concat deck [{:rank "Joker" :color "Red"
+                 :rank "Joker" :color "Black"}]))
+(defn shuffled
+  "Create a shuffled playing card deck."
+  [& {:keys [with-jokers] :or {with-jokers false}}]
+  (atom
+   (shuffle
+    (if with-jokers
+      (add-jokers (standard-deck))
+      (standard-deck)))))
+
+(defn card->str [{:keys [rank suit color] :as card}]
+  (if (= rank "Joker")
+    (format "%s Joker" color)
+    (format "%s of %s" rank suit)))
+
+(defn peek [deck n] (subvec @deck n))
+
+(defn draw! [deck n]
+  (let [cards (peek deck n)]
+    (swap! deck drop n)
+    cards))
